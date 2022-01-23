4dc73211c3ec6e5cadc7f59a83970573ea12f522
Brad Langhorst <langhorst@neb.com>
2022-01-22 21:42:27 -0500

    reorganizes functions by their call order

--- src/main.rs
+++ src/main.rs
@@ -43,38 +43,6 @@ struct PrimerSet {
     frac_consistent: f32,
 }
 
-/// checks the passed input structure for reasonableness, printing error if necessary.
-fn check_inputs(args: &Cli) -> Result<(), anyhow::Error> {
-    let mut error_messages = Vec::new();
-
-    if args.reads.exists() && args.primer_sets.iter().all(|ps| ps.exists()) {
-        log::info!(
-            "Searching for primers from {:?} in reads from: {:?}",
-            args.primer_sets,
-            args.reads
-        );
-    } else {
-        for ps in &args.primer_sets {
-            if !ps.exists() {
-                error_messages.push(format!("Could not find primer sets at {:?}", ps.as_path()));
-            }
-        }
-        if !args.reads.exists() {
-            error_messages.push(format!(
-                "Could not find reads at {:?}",
-                args.reads.as_path()
-            ));
-        }
-    }
-    if !error_messages.is_empty() {
-        log::error!("{}", error_messages.join("\n"));
-        //Err(anyhow!("Invalid input:{:?}", error_messages.join("\n")));
-        Err(anyhow!("Invalid input"))
-    } else {
-        Ok(())
-    }
-}
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Human Panic. Only enabled when *not* debugging.
     #[cfg(not(debug_assertions))]
@@ -109,44 +77,36 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-/// populates counts of primers observed in reads
-fn classify_reads(
-    reads_file: &PathBuf,
-    primer_set_counters: &mut Vec<PrimerSet>,
-) -> Result<(), anyhow::Error> {
-    let mut fastq_reader = File::open(reads_file.as_path())
-        .map(BufReader::new)
-        .map(noodles::fastq::Reader::new)
-        .with_context(|| anyhow!("Failed to open reads file: {:?}", reads_file.as_path()))?;
+/// checks the passed input structure for reasonableness, printing error if necessary.
+fn check_inputs(args: &Cli) -> Result<(), anyhow::Error> {
+    let mut error_messages = Vec::new();
 
-    Ok(for result in fastq_reader.records() {
-        let record = result?;
-        let read_seq = DnaString::from_acgt_bytes(record.sequence().as_ref());
-        if read_seq.len() < 16 {
-            log::warn!("skipping short read {:?}", read_seq);
-            break;
-        }
-        // extracts beginning and ending bases of each read
-        let left_key: Kmer16 = read_seq.slice(0, 16).get_kmer(0);
-        let right_key: Kmer16 = read_seq
-            .slice(read_seq.len() - 16, read_seq.len())
-            .get_kmer(0);
-
-        //primer_set_counters.par_iter_mut().for_each(|psc| {
-        for psc in &mut *primer_set_counters {
-            for key in [left_key, right_key] {
-                match psc.primer_counter.entry(key) {
-                    Entry::Occupied(val) => {
-                        *val.into_mut() += 1;
-                        psc.num_consistent_reads += 1
-                    }
-                    Entry::Vacant(_) => psc.num_inconsistent_reads += 1,
-                }
-                psc.frac_consistent = psc.num_consistent_reads as f32
-                    / (psc.num_consistent_reads + psc.num_inconsistent_reads) as f32
+    if args.reads.exists() && args.primer_sets.iter().all(|ps| ps.exists()) {
+        log::info!(
+            "Searching for primers from {:?} in reads from: {:?}",
+            args.primer_sets,
+            args.reads
+        );
+    } else {
+        for ps in &args.primer_sets {
+            if !ps.exists() {
+                error_messages.push(format!("Could not find primer sets at {:?}", ps.as_path()));
             }
         }
-    })
+        if !args.reads.exists() {
+            error_messages.push(format!(
+                "Could not find reads at {:?}",
+                args.reads.as_path()
+            ));
+        }
+    }
+    if !error_messages.is_empty() {
+        log::error!("{}", error_messages.join("\n"));
+        //Err(anyhow!("Invalid input:{:?}", error_messages.join("\n")));
+        Err(anyhow!("Invalid input"))
+    } else {
+        Ok(())
+    }
 }
 
 /// imports primer sets, creating primer_set_counter objects containing k-mers to search for
@@ -183,49 +143,6 @@ fn import_primer_sets(primer_set_paths: &Vec<PathBuf>) -> Result<Vec<PrimerSet>,
     }
     //TODO: compare all primer sets, removing ambiguous primers
     Ok(primer_set_counters)
-}
-
-/// summarizes primer set observations deciding which primer set was used
-fn identify_primer_set(primer_set_counters: Vec<PrimerSet>) -> String {
-    //TODO: add requirement that X fraction of primers have been observed allowing for some dropouts
-    // a primer set with fewer reads classified but with all primers represented is more confident 
-    // than one with more raw counts on fewer primers
-
-    //TODO: infer fragmentation or full length
-
-    //TODO: check that ratio of left and right are similar for each primer pair
-
-    let mut ps_fracs: Vec<(String, f32)> = Vec::new();
-    for psc in primer_set_counters {
-        log::debug!(
-            "{} consistent reads: {:?}",
-            psc.name,
-            psc.primer_counter
-                .iter()
-                .filter(|&(_, &count)| count > 0)
-                .collect::<HashMap<&Kmer16, &i64>>()
-        );
-        log::info!(
-            "{} inconsistent reads: {}",
-            psc.name,
-            psc.num_inconsistent_reads
-        );
-        ps_fracs.push((psc.name, psc.frac_consistent));
-    }
-
-    ps_fracs.sort_unstable_by_key(|ps_frac| (ps_frac.1 * -1000.0) as i32);
-    let ps_detected = if ps_fracs[0].1 > 0.0 {
-        if ps_fracs.len() == 1 || ps_fracs[1].1 <= 0.0 {
-            &ps_fracs[0].0
-        } else if ps_fracs.len() == 1 || ps_fracs[0].1 / ps_fracs[1].1 > 5.0 {
-            &ps_fracs[0].0
-        } else {
-            "unknown"
-        }
-    } else {
-        "unknown"
-    };
-    ps_detected.to_string()
 }
 
 /// Adds an 8-bit (16 nt) representation of the primer to the counter hash.
@@ -271,4 +188,87 @@ fn populate_primer_count_hash(
         }
     }
     Ok(())
+}
+
+/// populates counts of primers observed in reads
+fn classify_reads(
+    reads_file: &PathBuf,
+    primer_set_counters: &mut Vec<PrimerSet>,
+) -> Result<(), anyhow::Error> {
+    let mut fastq_reader = File::open(reads_file.as_path())
+        .map(BufReader::new)
+        .map(noodles::fastq::Reader::new)
+        .with_context(|| anyhow!("Failed to open reads file: {:?}", reads_file.as_path()))?;
+
+    Ok(for result in fastq_reader.records() {
+        let record = result?;
+        let read_seq = DnaString::from_acgt_bytes(record.sequence().as_ref());
+        if read_seq.len() < 16 {
+            log::warn!("skipping short read {:?}", read_seq);
+            break;
+        }
+        // extracts beginning and ending bases of each read
+        let left_key: Kmer16 = read_seq.slice(0, 16).get_kmer(0);
+        let right_key: Kmer16 = read_seq
+            .slice(read_seq.len() - 16, read_seq.len())
+            .get_kmer(0);
+
+        //primer_set_counters.par_iter_mut().for_each(|psc| {
+        for psc in &mut *primer_set_counters {
+            for key in [left_key, right_key] {
+                match psc.primer_counter.entry(key) {
+                    Entry::Occupied(val) => {
+                        *val.into_mut() += 1;
+                        psc.num_consistent_reads += 1
+                    }
+                    Entry::Vacant(_) => psc.num_inconsistent_reads += 1,
+                }
+                psc.frac_consistent = psc.num_consistent_reads as f32
+                    / (psc.num_consistent_reads + psc.num_inconsistent_reads) as f32
+            }
+        }
+    })
+}
+
+/// summarizes primer set observations deciding which primer set was used
+fn identify_primer_set(primer_set_counters: Vec<PrimerSet>) -> String {
+    //TODO: add requirement that X fraction of primers have been observed allowing for some dropouts
+    // a primer set with fewer reads classified but with all primers represented is more confident 
+    // than one with more raw counts on fewer primers
+
+    //TODO: infer fragmentation or full length
+
+    //TODO: check that ratio of left and right are similar for each primer pair
+
+    let mut ps_fracs: Vec<(String, f32)> = Vec::new();
+    for psc in primer_set_counters {
+        log::debug!(
+            "{} consistent reads: {:?}",
+            psc.name,
+            psc.primer_counter
+                .iter()
+                .filter(|&(_, &count)| count > 0)
+                .collect::<HashMap<&Kmer16, &i64>>()
+        );
+        log::info!(
+            "{} inconsistent reads: {}",
+            psc.name,
+            psc.num_inconsistent_reads
+        );
+        ps_fracs.push((psc.name, psc.frac_consistent));
+    }
+
+    ps_fracs.sort_unstable_by_key(|ps_frac| (ps_frac.1 * -1000.0) as i32);
+    let ps_detected = if ps_fracs[0].1 > 0.0 {
+        if ps_fracs.len() == 1 || ps_fracs[1].1 <= 0.0 {
+            &ps_fracs[0].0
+        } else if ps_fracs.len() == 1 || ps_fracs[0].1 / ps_fracs[1].1 > 5.0 {
+            &ps_fracs[0].0
+        } else {
+            "unknown"
+        }
+    } else {
+        "unknown"
+    };
+    ps_detected.to_string()
 }
